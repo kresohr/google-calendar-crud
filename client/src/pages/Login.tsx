ff49e9b90d1dbfbefe6f7e3247fe1a855eb46e2a
kresohr <ilicic.kresimir@gmail.com>
2025-10-25 11:34:32 +0200

    feat: implemented Login page with auth

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,28 @@
+import { useSearchParams } from "react-router-dom";
+import style from "./Login.module.css";
+
+// NOTE: If the URL is set different than localhost:3000, CORS errors and Google API errors may occur
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 export const Login = () => {
-  return <div>Login</div>;
+  const [searchParams] = useSearchParams();
+  const error = searchParams.get("error");
+  const handleGoogleLogin = () => {
+    window.location.href = `${API_URL}/auth/google`;
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      {error && (
+        <p className={style["error-text"]}>
+          {error === "auth_failed" &&
+            "Authentication failed. Please try again."}
+        </p>
+      )}
+
+      <button onClick={handleGoogleLogin}>Login with Google</button>
+    </div>
+  );
 };
