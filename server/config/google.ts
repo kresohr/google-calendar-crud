5f9b9ee663653b8ebab983f2800dae01e6064459
kresohr <ilicic.kresimir@gmail.com>
2025-10-25 11:07:09 +0200

    feat: define google oauth2Client and its scopes

---
+++ server/config/google.ts
@@ -0,0 +1,16 @@
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+
+export const SCOPES = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/calendar",
+];
