46a1766754f7f61a49aad7a029eab4eff0db3950
kresohr <ilicic.kresimir@gmail.com>
2025-10-25 11:35:02 +0200

    feat: implemented auth guard to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,26 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
 import "./App.css";
 
 export default function App() {
-  if (false) {
-    return (
-      <>
-        <h1>Google Calendar Crud</h1>
-        <main>
-          <Outlet />
-        </main>
-      </>
-    );
-  } else {
+  const { user, loading, logout } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
+
+  return (
+    <>
+      <h1>Google Calendar Crud</h1>
+      <p>Logged in as: {user.email}</p>
+      <button onClick={logout}>Logout</button>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
