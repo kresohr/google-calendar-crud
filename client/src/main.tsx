c0ff988801f9b473123c5b9e6beec5188b21eef2
kresohr <ilicic.kresimir@gmail.com>
2025-10-25 17:51:00 +0200

    feat: added Chakra and Toaster to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { Toaster } from "./components/ui/toaster";
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -10,9 +12,12 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AppRoutes />
-      </QueryClientProvider>
+      <ChakraProvider value={defaultSystem}>
+        <QueryClientProvider client={queryClient}>
+          <AppRoutes />
+          <Toaster />
+        </QueryClientProvider>
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
 );
